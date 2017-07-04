d1376e09efdb5efe5f183a3103a47c7448a052a2
LucashP <pula.lukasz@gmail.com>
2017-07-04 21:58:43 +0200

    Minimal configuration for Jetty server without web.xml

--- src/main/java/pl/lucash/resteasy/ResteasyApplication.java
+++ src/main/java/pl/lucash/resteasy/ResteasyApplication.java
@@ -1,9 +1,11 @@
 package pl.lucash.resteasy;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
+@ApplicationPath("/")
 public class ResteasyApplication extends Application {
 
     private Set<Object> singletons = new HashSet<Object>();
